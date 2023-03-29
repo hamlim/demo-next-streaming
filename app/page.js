3f8efc9dd9c02ac78bbebfba81ff48e9ddc267a7
Matt Hamlin <matthewjameshamlin@gmail.com>
2023-03-29 07:29:43 -0400

    Setup

--- app/page.js
+++ app/page.js
@@ -1,91 +1,72 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Suspense } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+export const config = { runtime: "edge" };
+export const revalidate = 0;
 
-export default function Home() {
+async function Product({ data }) {
+  const product = await data.then((res) => res.json());
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <p
+      style={{
+        background: (() => {
+          switch (product.id) {
+            case "1":
+              return "lightgreen";
+            case "2":
+              return "pink";
+            case "3":
+              return "dodgerblue";
+            default:
+              return "cyan";
+          }
+        })(),
+      }}
+    >
+      {product.id} - {product.name} {new Array(2500).fill(".").join("")}
+    </p>
+  );
+}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+export default async function Web() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-purple-700">Web</h1>
+      <Product
+        data={fetch(`https://app-dir.vercel.app/api/products?id=1&delay=400`, {
+          cache: "no-store",
+        })}
+      />
+      <Suspense fallback={<p>Loading Product 2 (3s)...</p>}>
+        <Product
+          data={fetch(
+            `https://app-dir.vercel.app/api/products?id=2&delay=3000`,
+            {
+              cache: "no-store",
+            }
+          )}
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </Suspense>
+      <Suspense fallback={<p>Loading Product 3 (5s)...</p>}>
+        <Product
+          data={fetch(
+            `https://app-dir.vercel.app/api/products?id=3&delay=5000`,
+            {
+              cache: "no-store",
+            }
+          )}
+        />
+      </Suspense>
+      <Suspense fallback={<p>Loading Product 4 (10s)...</p>}>
+        <Product
+          data={fetch(
+            `https://app-dir.vercel.app/api/products?id=4&delay=10000`,
+            {
+              cache: "no-store",
+            }
+          )}
+        />
+      </Suspense>
+    </div>
+  );
 }
